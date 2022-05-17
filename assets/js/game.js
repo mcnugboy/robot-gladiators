72c97ea8403a7d7df00cd13735729ec80f350868
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-05-16 23:12:41 -0500

    Game code updated.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,47 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("Whats is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-//this creates a function named "fight"
+// you can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-    window.alert("The fight has begun!")
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome To Robot Gladiators!");
+//subtract 
+enemyHealth = enemyHealth - playerAttack;
+
+//console message so we know it worked
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+);
+
+//check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+//subtract
+playerHealth = playerHealth - enemyAttack;
+
+//console message so we know it worked
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaming."
+);
+
+//check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
 }
 
-// fight();
+};
 
-var playerName = window.prompt("What is your robot's name?");
-// not the lack of quotations arounf playerName
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message.");
-// this will do math and log 20
-console.log(10+10);
-// what is this?
-console.log("Our robot's name is " + playerName);
-
-
+fight();
